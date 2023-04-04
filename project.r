21e77526b050e1f9f3bfc53c0e3ad1efaf5ac7ce
Daniel Cazarez <60197297+danielcaz2200@users.noreply.github.com>
2023-04-03 21:43:08 -0700

    Update project.r

--- project.r
+++ project.r
@@ -24,12 +24,13 @@ demographics <- demographics %>% select(-`Series Name`)
 covid_data <- covid_data %>% select(-c(total_deaths, new_deaths, total_deaths_per_million, new_deaths_per_million, new_deaths_smoothed_per_million))
 covid_data <- covid_data %>% select(-c(excess_mortality_cumulative_absolute, excess_mortality_cumulative, excess_mortality, excess_mortality_cumulative_per_million))
 
+covid_data %>% View()
 # Add new column new_deaths_smoothed_2wk that has the same values as 
 # new_deaths_smoothed but two weeks ahead
 covid_data <- covid_data %>%
   mutate(new_deaths_smoothed_2wk = lead(new_deaths_smoothed, n=14))
 
 # Tidy demographic table as needed
-demographics <- demographics %>% pivot_wider(names_from = `Series Code`, values_from = YR2015) 
+demographics <- demographics %>% pivot_wider(names_from = `Series Code`, values_from = YR2015)
 
 covid_data %>% inner_join(demographics, by=c(iso_code = "Country Code", location = "Country Name"))
